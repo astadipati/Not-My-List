70c35846a47030cb11ea34833f58f13ed2c6928c
rama astadipati <astadipati@outlook.com>
2018-05-01 11:24:34 +0700

    Props ke 2

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 export default class Header extends Component{
-
     render(){
+        // definisikan dulu jenis 2 props
+        const color = this.props.color;
+        const title = this.props.title;
+
         return(
-        <View style={{backgroundColor: this.props.color}}>
-            <Text>{this.props.title}</Text> 
-            {/* props nya melempar title */}
+        <View style={{backgroundColor: color}}>
+        {/* karena color sudah berupa object beda dengan title */}
+            <Text>{title}</Text> 
+            {/* props nya melempar title harus berupa object */}
         </View>
         );
     }
