fe74c484173ef6ee5fc80ef94de21262fa3efc37
rama astadipati <astadipati@outlook.com>
2018-05-01 11:31:16 +0700

    Jika di props nilainya int atau bool

--- App.js
+++ App.js
@@ -6,12 +6,12 @@ import Header from './components/Header';
 import Body from './components/Body';
 
 // cara panggil lebih simpel
-// const color = this.props.color;
-// const title = this.props.title;
+
 
 export default ()=> (
     <View style={{flex:1, justifyContent:'center'}}>
-      <Header title="My not list" color="yellow"/>
+    {/* jika props yang diambil berupa string atau boolean harus jadi object dulu*/}
+      <Header title={1} color="yellow"/>
       {/* ngambil string title yang dapat di isi value tertentu */}
       <Body/>
     </View>
