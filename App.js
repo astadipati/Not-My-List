79f2ab272ff185e824d10ae712890b02cd7d78db
rama astadipati <astadipati@outlook.com>
2018-05-01 10:07:57 +0700

    Functional

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
-export default class App extends Component {
-
-  render(){
-    return(
-      <View style={{flex:1, alignSelf: 'center', justifyContent: 'center'}}>
-      <Text>Hello dari component</Text>
-      </View>
-    )
-  }
+export default App = ()=> {
+  return
+    ( //es6 arrow function yang mereturn view
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>Hello Ini dari Functional</Text>
+    </View>
+  );
 }
