30d739e8b7cab39103e62787cc24211a0a9b9c30
rama astadipati <astadipati@outlook.com>
2018-05-02 19:58:43 +0700

    Loop Array biasa

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Content, Container, Text, Header, Body, List, ListItem, Left, CheckBox 
 export default class App extends Component{
 
   todos =['First todo', 'Second todo', 'Third Todo'];
+
   render(){
     return(
       // didalam container wajib
@@ -13,30 +14,25 @@ export default class App extends Component{
             <Text>Not to do list</Text>
           </Body>
         </Header>
+        {/* end header */}
+        {/* content */}
         <Content>
           <List>
+            {/* loop dimari pakai simple es6 ngambil nilai array todos yang dilempar ke objek baru todo*/}
+            {/* dan selanjutnya dilempar ke view text */}
+            {this.todos.map((todo) =>(
+              <ListItem>
+                <Left>
+                  {/* <Text>v</Text> kita ganti dengan checkbox asli*/}
+                  <CheckBox checked={false}/>
+                </Left>
+                {/* jangan lupa ditaruh lagi dalam body */}
+                <Body>
+                  <Text>{todo}</Text>
+                </Body>  
+              </ListItem>
+            ))}
 
-            <ListItem>
-              <Left>
-                {/* <Text>v</Text> kita ganti dengan checkbox asli*/}
-                <CheckBox checked={false}/>
-              </Left>
-              {/* jangan lupa ditaruh lagi dalam body */}
-              <Body>
-                <Text>First Item</Text>
-              </Body>  
-            </ListItem>
-            <ListItem>
-
-              <Left>
-                {/* <Text>v</Text> kita ganti dengan checkbox asli*/}
-                <CheckBox checked={true}/>
-              </Left>
-              {/* jangan lupa ditaruh lagi dalam body */}
-              <Body>
-                <Text>Second Item</Text>
-              </Body>  
-            </ListItem>
           </List>
         </Content>
       </Container>
