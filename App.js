101aa07c098636c1fdb7ddb499cc9a0e082b4753
rama astadipati <astadipati@outlook.com>
2018-05-02 20:07:39 +0700

    Loop data array object dan key id

--- App.js
+++ App.js
@@ -3,7 +3,20 @@ import { Content, Container, Text, Header, Body, List, ListItem, Left, CheckBox 
 
 export default class App extends Component{
 
-  todos =['First todo', 'Second todo', 'Third Todo'];
+  todos =[
+    {
+      id: "1",
+      notodo: "First Not todo"
+    },
+    {
+      id: "2",
+      notodo: "Second Not todo"
+    },
+    {
+      id: "3",
+      notodo: "Third Not todo"
+    },
+  ];
 
   render(){
     return(
@@ -21,14 +34,16 @@ export default class App extends Component{
             {/* loop dimari pakai simple es6 ngambil nilai array todos yang dilempar ke objek baru todo*/}
             {/* dan selanjutnya dilempar ke view text */}
             {this.todos.map((todo) =>(
-              <ListItem>
+              // jangan lupa key untuk menghilangkan error, karena parameter ini harus terdeteksi unik ketika di loop
+              <ListItem key={todo.id}>
                 <Left>
                   {/* <Text>v</Text> kita ganti dengan checkbox asli*/}
                   <CheckBox checked={false}/>
                 </Left>
                 {/* jangan lupa ditaruh lagi dalam body */}
                 <Body>
-                  <Text>{todo}</Text>
+                  {/* setelah kita aliaskan todo, baru ambil array objectnya */}
+                  <Text>{todo.notodo}</Text>
                 </Body>  
               </ListItem>
             ))}
