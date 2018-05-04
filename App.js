f00c3285063f3a77139f1d853f0c75f01fc3561a
rama astadipati <astadipati@outlook.com>
2018-05-04 23:35:15 +0700

    Flatlist

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
 import React, {Component} from 'react';
 import { Content, Container, Text, Header, Body, List, ListItem, Left, CheckBox } from 'native-base';
+import {FlatList} from 'react-native';
 import TodoItem from './components/TodoItem';
 export default class App extends Component{
 
   todos =[
     {
       id: "1",
-      notodo: "First Not todo"
+      notodo: "Makan"
     },
     {
       id: "2",
-      notodo: "Second Not todo"
+      notodo: "Mancing"
     },
     {
       id: "3",
-      notodo: "Third Not todo"
+      notodo: "Ngupil"
     },
   ];
 
-
+  _keyExtractor = (item, index) => item.id;
 
   render(){
     return(
@@ -33,7 +34,12 @@ export default class App extends Component{
         {/* content */}
         <Content>
           <List>
-            {this.todos.map((todo) =><TodoItem todo={todo} key={todo.id}/> )}
+            <FlatList
+              data={this.todos}
+              keyExtractor={this._keyExtractor}
+              renderItem={({item})=> <TodoItem todo={item}/>}
+            />
+            {/* {this.todos.map((todo) =><TodoItem todo={todo} key={todo.id}/> )} */}
           </List>
         </Content>
       </Container>
